d756b1233c29c25d3020da210dd2da4768d22ec9
jvijayakumar <jvijaya_kumar@yahoo.com>
2019-01-29 18:01:36 -0800

    Update HBaseDeregStream.scala

--- src/main/scala/sparkstream/HBaseDeregStream.scala
+++ src/main/scala/sparkstream/HBaseDeregStream.scala
@@ -25,12 +25,12 @@ import org.apache.hadoop.mapreduce.Job
 import net.liftweb.json._
 
 object HBaseDeregStream extends Serializable {
-  final val tableName = "nike_user_dereg"
+  final val tableName = "user_dereg"
   final val cfDeregBytes = Bytes.toBytes("dereg")
-  final val colUpmBytes = Bytes.toBytes("upm_id")
+  final val colUpmBytes = Bytes.toBytes("user_id")
   final val colDateBytes = Bytes.toBytes("dereg_date")
   // schema for Dereg data   
-  case class Dereg(upm_id: String, dereg_date: String)
+  case class Dereg(user_id: String, dereg_date: String)
   case class Event(id: String, event_time: String,event_id:String,dtrace_id:String)
   case class Dereg_events(event_details: Event)
   implicit val formats = DefaultFormats 
@@ -40,14 +40,14 @@ object HBaseDeregStream extends Serializable {
     def parseDereg(str: String): Dereg = {
       val parsedJson = parse(str)
       val a=parsedJson.extract[Dereg_events]
-      val upm_id= a.event_details.id
+      val user_id= a.event_details.id
       val dereg_date=a.event_details.event_time
-      Dereg(upm_id, dereg_date)
+      Dereg(user_id, dereg_date)
     }
     //  Convert a row of Dereg object data to an HBase put object
     def convertToPut(dereg: Dereg): (ImmutableBytesWritable, Put) = {
       // create a composite row key 
-      val rowkey = dereg.upm_id
+      val rowkey = dereg.user_id
       val put = new Put(Bytes.toBytes(rowkey))
       // add to column family data, column  data values to put object 
       put.add(cfDeregBytes, colDateBytes, Bytes.toBytes(dereg.dereg_date))
