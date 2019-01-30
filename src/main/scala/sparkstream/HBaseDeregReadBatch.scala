69e94b3062c8d303f50514aacec08be22b21e0fa
jvijayakumar <jvijaya_kumar@yahoo.com>
2019-01-29 17:52:37 -0800

    Add files via upload

---
+++ src/main/scala/sparkstream/HBaseDeregReadBatch.scala
@@ -0,0 +1,86 @@
+/*
+ * Spark read from hbase and write to parquet
+ *  
+ */
+
+package sparkstream
+
+import org.apache.hadoop.hbase.client.HBaseAdmin
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat
+import org.apache.hadoop.hbase.client.Put
+import org.apache.hadoop.hbase.client.HTable
+import org.apache.hadoop.hbase.HColumnDescriptor
+import org.apache.spark._
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.{HBaseConfiguration,HConstants, HTableDescriptor}
+import org.apache.hadoop.hbase.HColumnDescriptor
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.client.HBaseAdmin
+import org.apache.hadoop.fs.Path; 
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+
+object HBaseDeregReadBatch {
+ def main(args: Array[String]) {
+
+  if (args.length != 1) {
+        println("pass the parameters <zookeeper_quorum> <s3_path> ")
+    }
+    // set up HBase Table configuration
+    val conf = HBaseConfiguration.create()
+    val tableName = "nike_user_dereg"
+    val s3_path = args(1)
+
+    val confPathString = "/usr/lib/hbase/conf/hbase-site.xml" 
+    conf.addResource(new Path(confPathString)) 
+    //conf.set(HConstants.ZOOKEEPER_QUORUM, "10.184.216.115")
+    conf.set(HConstants.ZOOKEEPER_QUORUM, args(0))
+    
+
+    //Configuration setting for getting hbase data
+    conf.set(TableInputFormat.INPUT_TABLE, tableName)
+    conf.set("hbase.mapred.outputtable", tableName)
+    conf.set("mapreduce.outputformat.class", "org.apache.hadoop.hbase.mapreduce.TableOutputFormat");
+
+    val sparkConf = new SparkConf().setAppName("HBaseReadNike_user_dereg")
+    val sc = new SparkContext(sparkConf)
+    val spark = new org.apache.spark.sql.hive.HiveContext(sc)
+
+    // For implicit conversions like converting RDDs to DataFrames
+    import spark.implicits._
+
+    val hBaseRDD = sc.newAPIHadoopRDD(conf, classOf[TableInputFormat],
+    classOf[org.apache.hadoop.hbase.io.ImmutableBytesWritable],
+    classOf[org.apache.hadoop.hbase.client.Result])
+
+    //create RDD of result
+    val resultRDD =hBaseRDD.map(x => x._2 )
+
+    val key = resultRDD.map(result => Bytes.toString(result.getRow()))
+    //key.take(2).foreach(kv => println(kv)) 
+
+    //read individual column information from RDD
+    val deregRDD = resultRDD.map(result => Row(Bytes.toString(result.getRow()),
+      Bytes.toString(result.getValue(Bytes.toBytes("dereg"), Bytes.toBytes("dereg_date")))))
+
+
+    // The schema is encoded in a string
+    val schemaString = "upm_id dereg_date"
+
+    // Generate the schema based on the string of schema
+    val fields = schemaString.split(" ").map(fieldName => StructField(fieldName, StringType, nullable = true))
+    val schema = StructType(fields)
+    // Apply the schema to the RDD
+    val df = spark.createDataFrame(deregRDD, schema)
+
+    //deregRDD.take(2).foreach(kv => println(kv)) 
+        
+    //Save output to Hadoop
+    //deregRDD.saveAsTextFile(args(1))
+
+    //Save output to Hadoop parquet
+    df.write.mode("overwrite").parquet(s3_path)
+
+    sc.stop()
+ }
+}
---
+++ src/main/scala/sparkstream/HBaseDeregStreamWithCheckpoint.scala
@@ -0,0 +1,122 @@
+/*
+ * Spark streaming to ingest identity deregistered data to hbase
+ *  
+ */
+
+package sparkstream
+
+import org.apache.hadoop.hbase.client.Put
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+import org.apache.hadoop.hbase.mapreduce.TableOutputFormat
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.mapred.JobConf
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.Seconds
+import org.apache.spark.streaming.StreamingContext
+import org.apache.hadoop.hbase.client.HTable
+import org.apache.hadoop.hbase.{HBaseConfiguration,HConstants, HTableDescriptor}
+import org.apache.hadoop.hbase.HColumnDescriptor
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.client.HBaseAdmin
+import org.apache.hadoop.fs.Path; 
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat 
+import org.apache.hadoop.hbase.client.Put; 
+import org.apache.hadoop.mapreduce.Job
+import net.liftweb.json._
+
+object HBaseDeregStreamWithCheckpoint extends Serializable {
+  final val tableName = "nike_user_dereg"
+  final val cfDeregBytes = Bytes.toBytes("dereg")
+  final val colUpmBytes = Bytes.toBytes("upm_id")
+  final val colDateBytes = Bytes.toBytes("dereg_date")
+  // schema for Dereg data   
+  case class Dereg(upm_id: String, dereg_date: String)
+  case class Event(id: String, event_time: String,event_id:String,dtrace_id:String)
+  case class Dereg_events(event_details: Event)
+  implicit val formats = DefaultFormats 
+  
+  object Dereg extends Serializable  {
+    // function to parse line of Dereg data into Dereg class
+    def parseDereg(str: String): Dereg = {
+      val parsedJson = parse(str)
+      val a=parsedJson.extract[Dereg_events]
+      val upm_id= a.event_details.id
+      val dereg_date=a.event_details.event_time
+      Dereg(upm_id, dereg_date)
+    }
+    //  Convert a row of Dereg object data to an HBase put object
+    def convertToPut(dereg: Dereg): (ImmutableBytesWritable, Put) = {
+      // create a composite row key 
+      val rowkey = dereg.upm_id
+      val put = new Put(Bytes.toBytes(rowkey))
+      // add to column family data, column  data values to put object 
+      put.add(cfDeregBytes, colDateBytes, Bytes.toBytes(dereg.dereg_date))
+      return (new ImmutableBytesWritable(Bytes.toBytes(rowkey)), put)
+ 
+    }
+
+  }
+
+
+  def createSC(zookeeper_ip:String, inputDir:String,checkpointDir:String ): StreamingContext = {
+
+    // set up HBase Table configuration
+    val conf = HBaseConfiguration.create()
+
+    val confPathString = "/usr/lib/hbase/conf/hbase-site.xml" 
+    conf.addResource(new Path(confPathString)) 
+    conf.set(HConstants.ZOOKEEPER_QUORUM, zookeeper_ip)
+    conf.set("hbase.mapred.outputtable", tableName)
+    conf.set("mapreduce.outputformat.class", "org.apache.hadoop.hbase.mapreduce.TableOutputFormat");
+
+    val admin = new HBaseAdmin(conf) 
+
+    val sparkConf = new SparkConf().setAppName("HBaseStream")
+
+    val ssc = new StreamingContext(sparkConf,Seconds(5))
+
+    // Add checkpoint directory
+    ssc.checkpoint(checkpointDir)
+
+    // parse the lines of data into Dereg objects
+    val dereg = ssc.textFileStream(inputDir)
+
+    val deregDStream = dereg.map(Dereg.parseDereg)
+ 
+    deregDStream.print()
+
+    deregDStream.foreachRDD {rdd =>
+        rdd.map(Dereg.convertToPut).
+          saveAsNewAPIHadoopDataset(conf) }
+
+    ssc
+  }
+
+  def main(args: Array[String]) {
+
+    println(args.length)
+
+    if (args.length != 3) {
+        println("pass the parameters <zookeeper_quorum> <s3_input_filestream_path> <s3_checkpoint_path>")
+        System.exit(1)
+    }
+
+    println(args(0))
+    println(args(1))
+    println(args(2))
+    
+    val zookeeper_ip = args(0)
+    val inputDir = args(1)
+    val checkpointDir = args(2)
+    
+    // create a StreamingContext, the main entry point for all streaming functionality
+    val ssc = StreamingContext.getOrCreate(checkpointDir,() => createSC(zookeeper_ip,inputDir,checkpointDir))
+   
+    // Start the computation
+    ssc.start()
+    // Wait for the computation to terminate
+    ssc.awaitTermination()
+
+  }
+
+}
